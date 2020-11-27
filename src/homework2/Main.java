83daa4616119f3a75ca3b21a4e7469e77ff993d1
Илья Касаткин <IKKasatkin@mai.ru>
2020-11-27 17:19:28 +0300

    Добавил 7 задание

--- src/homework2/Main.java
+++ src/homework2/Main.java
@@ -11,6 +11,7 @@ public class Main {
         min_maxNumber();
         System.out.println();
         System.out.println(checkBalance(new int[]{2, 2, 2, 1, 3, 2, 11, 1}));
+        System.out.println(Arrays.toString(shift(new int[]{3,5,6,1}, -2)));
     }
 
     private static void invertArray() {
@@ -50,11 +51,11 @@ public class Main {
     }
 
     private static void fillDiagonal() {
-        int[][] arr = new int[4][4];
+        int[][] arr = new int[5][5];
         printArray(arr);
-        for (int i = 0; i < 4; i++) {
-            for(int j = 0; j < 4; j++){
-                if(i == j){
+        for (int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr.length; j++){
+                if(i == j || (i + j) == arr.length-1){
                     arr[i][j] = 1;
                 }
             }
@@ -100,5 +101,23 @@ public class Main {
             }
         }
         return false;
+    }
+
+    private static int[] shift(int[] arr, int n){
+        System.out.println(Arrays.toString(arr));
+        if(n > 0) {
+            for (int j = 0; j < 1; j++) {
+                int buffer = arr[arr.length - 1];
+                System.arraycopy(arr, 0, arr, 1, arr.length - 1);
+                arr[0] = buffer;
+            }
+        }else {
+            for(int j = 0; j < -n; j++) {
+                int buffer = arr[0];
+                System.arraycopy(arr, 1, arr, 0, arr.length - 1);
+                arr[arr.length - 1] = buffer;
+            }
+        }
+        return arr;
     }
 }
