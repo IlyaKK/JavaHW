a468f963cd42ab0f3840864257cf5ed223af75db
Ilya Kasatckin <43324144+IlyaKK@users.noreply.github.com>
2020-12-05 04:30:04 +0300

    Update Main.java
    Опечатался

--- src/homework2/Main.java
+++ src/homework2/Main.java
@@ -106,7 +106,7 @@ public class Main {
     private static int[] shift(int[] arr, int n){
         System.out.println(Arrays.toString(arr));
         if(n > 0) {
-            for (int j = 0; j < 1; j++) {
+            for (int j = 0; j < n; j++) {
                 int buffer = arr[arr.length - 1];
                 System.arraycopy(arr, 0, arr, 1, arr.length - 1);
                 arr[0] = buffer;
