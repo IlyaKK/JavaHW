3a855a870759abdecf593f9eb10924ad1fd38140
IlyaK <ilja.kasatckin@yandex.ru>
2020-12-12 23:51:50 +0300

    Создал класс "Сотрудник", выполнив все задания

--- src/homework5/Person.java
+++ src/homework5/Person.java
@@ -1,2 +1,36 @@
-package homework5;public class Person {
+package homework5;
+
+public class Person {
+    private String name;
+    private String post;
+    private String mail;
+    private String phoneNumber;
+    private long salary;
+    private int age;
+
+    public Person(String name, String post, String mail, String phoneNumber, long salary, int age) {
+        this.name = name;
+        this.post = post;
+        this.mail = mail;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public String toString() {
+        return "Сотрудник: " + name + '\n' +
+                " Должность: " + post + '\n' +
+                " email: " + mail + '\n' +
+                " Телефон: " + phoneNumber + '\n' +
+                " Зарплата: " + salary + '\n' +
+                " Возраст: " + age;
+    }
+
+    public void printInfo() {
+        System.out.println(this);
+    }
+
+    public long getAge() {
+        return age;
+    }
 }
